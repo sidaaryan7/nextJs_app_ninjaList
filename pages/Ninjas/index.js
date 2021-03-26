7aaa763de070146d71bd35c860673dfe45cc5181
rajeevpal03 <rajeev.pal@hsv.digital>
2021-03-26 14:31:22 +0530

    finalcommit

--- pages/Ninjas/index.js
+++ pages/Ninjas/index.js
@@ -1,26 +1,32 @@
-export const getStaticProps= async ()=>{
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data= await response.json();
 
-    return {
-        props:{ninjas:data}
-    }
+ 
+import styles from '../../styles/Ninjas.module.css'
+import Link from 'next/link'
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data }
+  }
 }
 
-const Ninjas = ({ninjas}) => {
-    return (
-        <div>
-            <h1>all ninjas</h1>
-            {ninjas.map(ninja=>(
-                <div key={ninja.id}>
-                    <a>
-                        <h3>{ninja.name}</h3>
-                        
-                    </a>
-                </div>
-            ))}
-        </div>
-      );
+const Ninjas = ({ ninjas }) => {
+  // console.log(ninjas)
+
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map(ninja => (
+        <Link href={'/Ninjas/' + ninja.id} key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ ninja.name }</h3>
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
 }
  
 export default Ninjas;
